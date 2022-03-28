167dea225a2d41d0189ddf172cbbcb88bd2841db
PortalUniverse <alexszczepucha@gmail.com>
2022-03-27 17:04:44 -0700

    hopefully final commit!

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include "Account.h"
 
 int main(){
-    Account account(Money(300,23));
+    
+	// account tests
+	Account account(Money(300,23));
     std::cout<< account <<std::endl;
 
     account.makeDeposit(Money(200,00));
@@ -14,6 +16,73 @@ int main(){
     account.makeWithdrawals(Money(300,10));
     account.makeWithdrawals(Money(201,34));
     std::cout<< account <<std::endl;
+
+
+	// money relational operator tests
+	Money m(4, 15);
+	Money n(3, 27);
+
+	std::cout << "Relational Operator Tests\n" << std::endl;
+
+	// if m < n returns false, pass test
+	if (!(m < n))
+	{
+		std::cout << "Test 1 Pass" << std::endl;
+	}
+	else
+	{
+		std::cout << "Test 1 Fail" << std::endl;
+	}
+
+	// if m > n returns true, pass test
+	if (m > n)
+	{
+		std::cout << "Test 2 Pass" << std::endl;
+	}
+	else
+	{
+		std::cout << "Test 2 Fail" << std::endl;
+	}
+
+	// if m <= n returns false, pass test
+	if (!(m <= n))
+	{
+		std::cout << "Test 3 Pass" << std::endl;
+	}
+	else
+	{
+		std::cout << "Test 3 Fail" << std::endl;
+	}
+
+	// if m >= n returns true, pass test
+	if (m >= n)
+	{
+		std::cout << "Test 4 Pass" << std::endl;
+	}
+	else
+	{
+		std::cout << "Test 4 Fail" << std::endl;
+	}
+
+	// if m != n returns true, pass test
+	if (m != n)
+	{
+		std::cout << "Test 5 Pass" << std::endl;
+	}
+	else
+	{
+		std::cout << "Test 5 Fail" << std::endl;
+	}
+
+	// if m == n returns false, pass test
+	if (!(m == n))
+	{
+		std::cout << "Test 6 Pass" << std::endl;
+	}
+	else
+	{
+		std::cout << "Test 6 Fail" << std::endl;
+	}
 
     return 0;
 }
